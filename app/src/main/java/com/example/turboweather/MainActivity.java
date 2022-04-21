47ad62f8bb6366ffab175b7bf96d2e36a417aeb3
SashaPotemkin <alex.potjomkin@gmail.com>
2022-04-21 19:28:44 +0300

    Добавил переменные в MainActivity

--- app/src/main/java/com/example/turboweather/MainActivity.java
+++ app/src/main/java/com/example/turboweather/MainActivity.java
@@ -2,10 +2,33 @@ package com.example.turboweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    final String APP_ID = "dab3af44de7d24ae7ff86549334e45bd";
+    final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 
+    final long MIN_TIME = 5000;
+    final float MIN_DISTANCE = 1000;
+    final int REQUEST_CODE = 101;
+
+
+    String Location_Provider = LocationManager.GPS_PROVIDER;
+
+    TextView NameofCity, weatherState, Temperature;
+    ImageView mweatherIcon;
+    ImageView Background;
+
+    RelativeLayout mCityFinder;
+
+
+    LocationManager mLocationManager;
+    LocationListener mLocationListner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
