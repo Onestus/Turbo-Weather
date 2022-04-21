9ded0ed84f15786a7b6f406ded7d2f9794947d83
SashaPotemkin <alex.potjomkin@gmail.com>
2022-04-21 19:32:43 +0300

    Добавил  javadoc комментарий к MainActivity

--- app/src/main/java/com/example/turboweather/MainActivity.java
+++ app/src/main/java/com/example/turboweather/MainActivity.java
@@ -9,6 +9,28 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+/**
+ * Здесь просиходит вся магия.
+ * <p>
+ * При открытии данного активити, пользователь увидит погоду по его текущему местоположению,
+ * а именно состояние погоды, название города, температуру в Цельсиях.
+ * <p>
+ * Здесь находится кнопка перехода на страницу поиска погоды по городу.
+ *<p>
+ *{@link String APP_ID} - ID приложение по которому сайт openweathermap.org передает состояние погоды.
+ * <p>
+ *{@link String WEATHER_URL} - URL сайта openweathermap.org, который передает состояние погоды.
+ <p>
+ *{@link String Location_Provider} - переменная, получающая местоположение пользователя.
+ * <p>
+ *{@link String NameofCity, weatherState, Temperature} - название города, состояние погоды и температура соответственно.
+ *<p>
+ *{@link String mweatherIcon} - иконка состояния погоды.
+ * <p>
+ *{@link String Background} - цвет фона.
+ * @see cityFinder
+ *
+ */
 public class MainActivity extends AppCompatActivity {
     final String APP_ID = "dab3af44de7d24ae7ff86549334e45bd";
     final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
