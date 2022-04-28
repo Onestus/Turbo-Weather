7fd518a6b4fb502fafb18ea56f4156a1bfa89aae
SashaPotemkin <alex.potjomkin@gmail.com>
2022-04-28 15:47:26 +0300

    Написал весь cityFinder

--- app/src/main/java/com/example/turboweather/cityFinder.java
+++ app/src/main/java/com/example/turboweather/cityFinder.java
@@ -1,4 +1,70 @@
 package com.example.turboweather;
 
-public class cityFinder {
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cityFinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_finder);
+        final EditText editText=findViewById(R.id.searchCity);
+        ImageView backButton=findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String newCity= editText.getText().toString();
+                Intent intent=new Intent(cityFinder.this,MainActivity.class);
+                intent.putExtra("City",newCity);
+                startActivity(intent);
+
+
+
+                return false;
+            }
+        });
+    }
+
+
+    public void OnClickSettings(View view) {
+        showWindow();
+    }
+
+
+    public void showWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Правила ввода города");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View signin_window=inflater.inflate(R.layout.activity_settings_window,null);
+        dialog.setView(signin_window);
+
+        dialog.setPositiveButton("Понятно", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+
+            }
+        });
+        dialog.show();
+    }
 }
