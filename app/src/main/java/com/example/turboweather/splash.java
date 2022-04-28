d6aaf6741f25440013ad325a29bcd3d5685fa2de
SashaPotemkin <alex.potjomkin@gmail.com>
2022-04-28 16:45:25 +0300

    Добавил Сплеш-скрин

---
+++ app/src/main/java/com/example/turboweather/splash.java
@@ -0,0 +1,25 @@
+package com.example.turboweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splash.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },1*1000);
+    }
+}
